aa88bf18105d24e228205224054f222a3bb598ac
ricebucketjoanne <F04046036@gs.ncku.edu.tw>
2019-08-26 15:27:22 +0800

    刪除到原本的樣子

--- src/hangman/Main.java
+++ src/hangman/Main.java
@@ -5,10 +5,5 @@ public class Main {
 	/*test*/
 	public static void main(String[] args) {
 		HangmanFrame hangman = new HangmanFrame();
-		System.out.println("asdfasdfwerwjerjwerhl");
-		System.out.println("are you okay???");
-		System.out.println("jajajajaaj");
-		System.out.println("123");
-		System.out.println("456");
 	}
 }
