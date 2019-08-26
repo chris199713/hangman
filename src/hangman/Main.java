491e3d1961251e68ea2e2d3d1ec5d5765545af97
ricebucketjoanne <F04046036@gs.ncku.edu.tw>
2019-08-26 10:36:30 +0800

    weeeeeeee

--- src/hangman/Main.java
+++ src/hangman/Main.java
@@ -6,5 +6,6 @@ public class Main {
 	public static void main(String[] args) {
 		HangmanFrame hangman = new HangmanFrame();
 		System.out.println("asdfasdfwerwjerjwerhl");
+		System.out.println("are you okay???");
 	}
 }
