1c0c9a66084bf330aeb7166e228357ba954b8952
Pin-Xian <40723225l@gapps.ntnu.edu.tw>
2019-08-26 10:11:30 +0800

    test

--- src/hangman/Main.java
+++ src/hangman/Main.java
@@ -2,7 +2,7 @@ package hangman;
 
 
 public class Main {
-	
+	/*test*/
 	public static void main(String[] args) {
 		HangmanFrame hangman = new HangmanFrame();
 		System.out.println("asdfasdfwerwjerjwerhl");
