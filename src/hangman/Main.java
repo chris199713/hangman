e489436dd3c25c909a6d48e6989c31c8d56810d8
Pin-Xian <40723225l@gapps.ntnu.edu.tw>
2019-08-26 10:52:34 +0800

    conflict

--- src/hangman/Main.java
+++ src/hangman/Main.java
@@ -6,10 +6,7 @@ public class Main {
 	public static void main(String[] args) {
 		HangmanFrame hangman = new HangmanFrame();
 		System.out.println("asdfasdfwerwjerjwerhl");
-<<<<<<< HEAD
 		System.out.println("are you okay???");
-=======
 		System.out.println("jajajajaaj");
->>>>>>> jij
 	}
 }
